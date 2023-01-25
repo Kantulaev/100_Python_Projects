eb3baac77a5bbcaf0584b1940a61a67d112a01a1
Kantulaev <Kantulaev@gmail.com>
2023-01-25 13:58:08 +0600

    day 1

---
+++ 1_day.py
@@ -0,0 +1,5 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+city_name = input('what is name of your city? ')
+pet_name = input('what is name of your pet? ')
+print('your band name is ' + city_name + ' ' + pet_name)
